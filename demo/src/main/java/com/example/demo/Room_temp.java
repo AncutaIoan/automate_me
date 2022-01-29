08ca2fa9d1586f3a6d6a67b356822a4e766cac87
OlteanuCosmin <50953863+OlteanuCosmin@users.noreply.github.com>
2022-01-30 00:20:57 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Room_temp.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Room_temp {
+    private final String id;
+    private final float average_temp;
+
+    public Room_temp(String id, float average_temp) {
+        this.id = id;
+        this.average_temp = average_temp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getAverage_temp() {
+        return average_temp;
+    }
+
+}
